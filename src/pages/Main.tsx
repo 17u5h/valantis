6cd21e71158dce4e35da69a590e0988ddb232765
17u5h <102058870+17u5h@users.noreply.github.com>
2024-02-22 16:37:42 +0700

    initial

---
+++ src/pages/Main.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import * as style from './style.module.scss'
+import Header from "../widgets/Header";
+
+const Main = () => {
+    return (
+        <div className="container">
+            <Header/>
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/widgets/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import * as style from './style.module.scss'
+
+const Header = () => {
+    return (
+        <div className="container">
+
+        </div>
+    );
+};
+
+export default Header;
